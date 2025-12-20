e93b4c4a9f5d6226530f065faaf21337649e4f39
Addilete <165387997+adillette@users.noreply.github.com>
2025-12-20 23:26:42 +0900

    fix: users 형식 변경

--- clothes-service/src/main/java/today/wishwordrobe/user/infrastructure/UserRepository.java
+++ clothes-service/src/main/java/today/wishwordrobe/user/infrastructure/UserRepository.java
@@ -9,7 +9,7 @@ import today.wishwordrobe.user.domain.Users;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<Users, Integer> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByEmail(String email);
 }
