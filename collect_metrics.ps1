83a860f3c3ebe65e8ecc05f3d8c4ed1768329597
Addilete <165387997+adillette@users.noreply.github.com>
2026-09-19 14:34:52 +0900

    test: 동기식 반복 메트릭 이용해서 부하 발생시키기 위하여 powershell 구문으로 반복 요청 csv 파일 만듦

---
+++ collect_metrics.ps1
@@ -0,0 +1,10 @@
+﻿$out = "D:\msaprocject\wishwardrobe-back\inflight_$(Get-Date -Format 'yyyyMMdd_HHmmss').csv"
+"timestamp,inflight" | Out-File $out -Encoding utf8
+while ($true) {
+    $ts = Get-Date -Format o
+    $text = (Invoke-WebRequest -Uri "http://localhost:9091/actuator/prometheus" -UseBasicParsing).Content
+    $line = $text -split "`n" | Where-Object { $_ -match '^broadcast_inflight_messages\s' }
+    $val = ($line -split '\s+')[1]
+    "$ts,$val" | Out-File $out -Append -Encoding utf8
+    Start-Sleep -Milliseconds 200
+}
