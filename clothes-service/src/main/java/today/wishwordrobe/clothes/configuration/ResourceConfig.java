0427e855457822bf21e6563a510c0465e83420c8
Addilete <165387997+adillette@users.noreply.github.com>
2026-09-23 14:39:07 +0900

    fix: yml에 있는 image 접근용 주소 통일함 기존 uploadDir-> uploadFiles 로 수정 핸들러안에 있는 주소 대문자-> 소문자로 수정                 .addResourceLocations("file:///"+fileDir + "/") //실제로 검색하는 로컬 디렉토리 "/" 없으면 디렉토리를 인식을 못해서 get으로 불러오기 불가함

--- clothes-service/src/main/java/today/wishwordrobe/clothes/configuration/ResourceConfig.java
+++ clothes-service/src/main/java/today/wishwordrobe/clothes/configuration/ResourceConfig.java
@@ -6,15 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class ResourceConfig implements WebMvcConfigurer {
-    @Value("${file.uploadDir}")
+    @Value("${file.uploadFiles}")
     String fileDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/Clothes/images/**")
-                .addResourceLocations("file:///"+fileDir)
-                .setCachePeriod(60*60*24*365); //점근파일 캐싱시간
+        registry.addResourceHandler("/clothes/images/**") //브라우저가 요청하는 url 패턴
+                .addResourceLocations("file:///"+fileDir + "/") //실제로 검색하는 로컬 디렉토리
+                .setCachePeriod(60*60*24*365); //접근파일 캐싱시간
     }
 }
